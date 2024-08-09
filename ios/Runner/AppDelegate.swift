852e07e5a2ec39b0e19e0abfc8d41288a948e9bc
Lovre <risa@rs22.mobi>
2024-08-09 14:45:27 +0800

    Move DB file from ApplicationSupport to AppDocuments

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,6 +11,30 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     
+      let dbFiles = ["database.isar", "database.isar.lock"]
+      
+      for dbFile in dbFiles {
+          let new_db_file = URL.documents.appendingPathComponent(dbFile)
+          let old_db_file = URL.applicationSupport.appendingPathComponent(dbFile)
+          
+          if(FileManager.default.fileExists(atPath: old_db_file.absoluteString)){
+              
+              if(!FileManager.default.fileExists(atPath: new_db_file.absoluteString) ||
+                 (fileModificationDate(url: old_db_file) > fileModificationDate(url: new_db_file))){
+                  
+                  do{
+                      try FileManager.default.moveItem(at: old_db_file, to: new_db_file)
+                      print("Database file \(dbFile) moved to AppDocuments")
+                  }catch{
+                      print("Move database file \(dbFile) fail")
+                  }
+                  
+              }
+          }
+      }
+      
+      
+      
       let defaults = UserDefaults.standard
       let agreeEULA = defaults.bool(forKey: "AgreeEULA")
       let agreeEULA_Ver = defaults.string(forKey: "AgreeEULA_Ver")
@@ -34,6 +58,18 @@ import Flutter
       
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    
+    func fileModificationDate(url: URL) -> Date {
+        do {
+            let attr = try FileManager.default.attributesOfItem(atPath: url.path)
+            return attr[FileAttributeKey.modificationDate] as? Date ?? Date.distantPast
+        } catch {
+            return Date.distantPast
+        }
+    }
+    
+    
 }
 
 extension UIApplication {
@@ -46,4 +82,20 @@ extension UIApplication {
     static var version: String {
         return "\(release).\(build)"
     }
+}
+
+extension URL {
+    
+    static var documents: URL {
+        return FileManager
+            .default
+            .urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+    
+    static var applicationSupport: URL {
+        return FileManager
+            .default
+            .urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+    }
+    
 }
